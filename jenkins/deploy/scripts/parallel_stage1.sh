688b3e7e006db875d35f7b9ad6f2a7ba7a43e907
Nicolas Richart <richart@ph02.phoenix.cluster>
2022-07-21 17:52:40 +0200

    Adding a Jenkinsfile as an example

---
+++ jenkins/deploy/scripts/parallel_stage1.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Running the 1st stage in parallel for env ${environment} of ${STACK_RELEASE}"
---
+++ jenkins/deploy/scripts/sequential_stage1.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Running the 1st stage in sequential for ${STACK_RELEASE}"
