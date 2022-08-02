c842b4301907e9d932d2721b9363f6a01bd93b67
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-02 15:09:47 +0200

    Jenkins: add update_configuration_script

---
+++ jenkins/deploy/scripts/update_production_configuration.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+set -euo pipefail
+
+# This script assumes that the following variables are set in the environment:
+#
+# SENV_VIRTUALENV_PATH: path where to setup the virtualenv for "senv"
+#
+#
+
+echo 'Installing Python virtual environment'
+mkdir -p ${PYTHON_VIRTUALENV_PATH}
+python3 -m venv ${PYTHON_VIRTUALENV_PATH}
+
+echo 'Activating Python virtual environment'
+. ${PYTHON_VIRTUALENV_PATH}/bin/activate
+
+echo 'Upgrading PIP'
+pip install -U pip
+
+echo 'Installing Jinja2'
+pip install jinja2
