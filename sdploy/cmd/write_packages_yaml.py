cb79aade73b46ae145cefad48fb0ae9f3dc69c2c
Nicolas Richart <nicolas.richart@epfl.ch>
2022-08-03 14:59:50 +0200

    makes write_packages also use active environment

--- sdploy/cmd/write_packages_yaml.py
+++ sdploy/cmd/write_packages_yaml.py
@@ -13,24 +13,20 @@
 #                                                                       #
 #                                                                       #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-import argparse
-import collections
-import sys
-import os
-
 import spack
 import spack.cmd
+import spack.config
+import spack.environment as ev
+import spack.schema.env
+import spack.util.spack_yaml as syaml
+import llnl.util.filesystem as fs
 
-from copy import deepcopy
 from jinja2 import Environment, FileSystemLoader
-from pdb import set_trace as st
 
 description = "write packages.yaml file"
 section = "Sdploy"
 level = "short"
 
-from ..yaml_manager import ReadYaml
 from ..packages_yaml import PackagesYaml
 from ..util import *
 from ..config import *
@@ -53,6 +49,13 @@ def setup_parser(subparser):
         '-d', '--debug', action='store_true', default=False,
         help='print debug information.'
     )
+
+def _write_yaml(output, filename):
+    with fs.write_tmp_and_move(os.path.realpath(filename)) as f:
+        yaml = syaml.load_config(output)
+        spack.config.validate(yaml, spack.schema.packages.schema, filename)
+        syaml.dump_config(yaml, f, default_flow_style=False)
+
 
 def write_packages_yaml(parser, args):
     """Create spack.yaml file"""
@@ -78,12 +81,16 @@ def write_packages_yaml(parser, args):
 
     # Set up jinja
     file_loader = FileSystemLoader(config.templates_path)
-    env = Environment(loader = file_loader, trim_blocks = True)
+    jinja2_env = Environment(loader = file_loader, trim_blocks = True)
 
     # Render and write packages.yaml
-    template = env.get_template(config.packages_yaml_template)
+    template = jinja2_env.get_template(config.packages_yaml_template)
     output = template.render(packages = data)
-    print(output)
-    with open(packages_yaml, 'w') as f:
-        f.write(output)
 
+    print(output)
+
+    env = ev.active_environment()
+    if env:
+        _write_yaml(output, os.path.realpath(env.manifest_path))
+    else:
+        _write_yaml(output, packages_yaml)
