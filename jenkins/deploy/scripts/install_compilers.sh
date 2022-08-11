be10fc53adf63274bc9fd550dcd22cd604983cd8
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-11 17:54:23 +0200

    jenkins debug

---
+++ jenkins/deploy/scripts/install_compilers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+set -euo pipefail
+
+echo 'Activating Spack'
+. $JENKINS_SCRIPTS_PATH/activate_spack.sh
+
+spack install-compilers
+
+env
+
--- sdploy/cmd/install_compilers.py
+++ sdploy/cmd/install_compilers.py
@@ -21,22 +21,79 @@ import spack.schema.env
 import spack.util.spack_yaml as syaml
 import llnl.util.filesystem as fs
 import llnl.util.tty as tty
+
 from spack.cmd.env import _env_create
 
-description = "create environments"
+from jinja2 import Environment, FileSystemLoader
+
+description = "install compilers"
 section = "Sdploy"
 level = "short"
 
 from ..util import *
 from ..config import *
+from ..spack_yaml import SpackYaml
+from ..config_manager import Config
 
-def create_environments(parser, args):
-    """Create environments"""
+def setup_parser(subparser):
+    subparser.add_argument(
+        '-s', '--stack',
+        help='path to the stack file'
+    )
+    subparser.add_argument(
+        '-p', '--platform',
+        help='path to the platform file.'
+    )
+    subparser.add_argument(
+        '-t', '--templates-path',
+        help='where to find jinja templates'
+    )
+    subparser.add_argument(
+        '-d', '--debug', action='store_true', default=False,
+        help='print debug information.'
+    )
 
-    envs_list = ReadYaml()
-    envs_list.read(fs.join_path(get_prefix(), 'stacks/common.yaml'))
+def _write_yaml(output, filename):
+    with fs.write_tmp_and_move(os.path.realpath(filename)) as f:
+        yaml = syaml.load_config(output)
+        spack.config.validate(yaml, spack.schema.env.schema, filename)
+        syaml.dump_config(yaml, f, default_flow_style=False)
 
-    for e in envs_list.data['environments']:
-        if e not in ev.all_environment_names():
-            tty.info(f'Environment {e} not found')
-            _env_create(e)
+def install_compilers(parser, args):
+    """INstall compilers"""
+
+    config = Config(args)
+    if config.debug:
+        config.info()
+
+    # Process Programming Environment section.
+    stack = SpackYaml(config.platform_yaml, config.stack_yaml, config.debug)
+
+    # Create PE definitions dictionary
+    stack.create_pe_definitions_dict()
+
+    # Create PE matrix dictionary
+    stack.create_pe_compiler_specs_dict()
+
+    # Concatenate all dicts
+    data = {}
+    data['pe_defs'] = stack.pe_defs
+    data['pe_specs'] = stack.pe_specs
+
+    # Jinja setup
+    file_loader = FileSystemLoader(config.templates_path)
+    jinja_env = Environment(loader = file_loader, trim_blocks = True)
+
+    # Render and write spack.yaml
+    template = jinja_env.get_template(config.spack_yaml_template)
+    output = template.render(stack = data)
+
+    tty.msg(config.spack_yaml)
+    print(output)
+
+    env = ev.active_environment()
+    if env:
+        _write_yaml(output, os.path.realpath(env.manifest_path))
+    else:
+        _write_yaml(output, config.spack_yaml)
+
