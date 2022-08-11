e1ac342d44d01a8cf36cefd8c6d5f073b195a2b9
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-11 18:53:31 +0200

    jenkins debug

--- jenkins/deploy/scripts/install_compilers.sh
+++ jenkins/deploy/scripts/install_compilers.sh
@@ -5,5 +5,5 @@ echo 'Activating Spack'
 . $JENKINS_SCRIPTS_PATH/activate_spack.sh
 
 spack install-compilers --templates stacks/syrah-lite/templates
-
+mv spack.yaml /home/scitasbuildpr/syrah/spack.v1/var/spack/environments/ph02-avx
 env
