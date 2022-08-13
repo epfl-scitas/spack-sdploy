4680a70bbdb550d3a8d877027c9aa2a3a82931c4
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-13 16:29:05 +0200

    jenkinsfile debug

--- jenkins/deploy/scripts/install_compilers.sh
+++ jenkins/deploy/scripts/install_compilers.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 echo 'Activating Spack'
 . $JENKINS_SCRIPTS_PATH/activate_spack.sh
 
-spack install-compilers --templates stacks/syrah-lite/templates
-mv spack.yaml /home/scitasbuildpr/syrah/spack.v1/var/spack/environments/ph02-avx
-env
+yareed -file stacks/common.yaml -keys environments
+
+#spack install-compilers --templates stacks/syrah-lite/templates
+#mv spack.yaml /home/scitasbuildpr/syrah/spack.v1/var/spack/environments/ph02-avx
+#env
