47abee190fa3d2d1d9ae760ff6d7eb8af4740ae1
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-14 17:33:08 +0200

    jenkins debug

---
+++ jenkins/deploy/scripts/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+# SCITAS STACK DEPLOYMENT 2022, EPFL                                          #
+#                                                                             #
+# This is a conveinience script that loads importante environment variables,  #
+# activates the Python environment and sets up Spack. In the end, it will     #
+# just run the command that it was given to as parameter.                     #
+#                                                                             #
+#                                                                             #
+#                                                                             #
+#                                                                             #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+echo 'Load variables'
+. ${JENKINS_SCRIPTS_PATH}/config.sh
+
+echo 'Activating Python virtual environment'
+. ${PYTHON_VIRTUALENV_PATH}/bin/activate
+
+echo 'Source Spack'
+. $SPACK_INSTALL_PATH/share/spack/setup-env.sh
+
+$@
