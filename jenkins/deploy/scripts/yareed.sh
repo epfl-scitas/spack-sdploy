c69d27db2ef9010803b354a27572210aa052df6a
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-15 11:14:55 +0200

    jenkins debug

---
+++ jenkins/deploy/scripts/yareed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                             #
+# SCITAS STACK DEPLOYMENT 2022, EPFL                                          #
+#                                                                             #
+# This is a conveinience script that loads importante environment variables,  #
+# activates the Python environment and sets up Spack. In the end, it will     #
+# just run the command that it was given to as parameter.                     #
+#                                                                             #
+#                                                                             #
+#                                                                             #
+#                                                                             #
+#                                                                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# 'Activating Python virtual environment'
+. ${PYTHON_VIRTUALENV_PATH}/bin/activate
+
+$@
