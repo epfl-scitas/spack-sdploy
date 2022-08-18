20600f663d5c5c19a644a5b37d0feafbfd91c0bc
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-18 17:04:10 +0200

    remove --env from spack commands

--- jenkins/deploy/scripts/deploy_spack_environment.sh
+++ jenkins/deploy/scripts/deploy_spack_environment.sh
@@ -7,7 +7,7 @@ echo "Installing stack configuration: ${environment}"
 spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment}
 
 echo "Installing modules configuration: ${environment}"
-spack --env ${environment} write-modules-yaml -s ${STACK_RELEASE} -p ${environment}
+spack write-modules-yaml -s ${STACK_RELEASE} -p ${environment}
 
 echo "Installing packages configuration: ${environment}"
-spack --env ${environment} write-packages-yaml -s ${STACK_RELEASE} -p ${environment}
+spack write-packages-yaml -s ${STACK_RELEASE} -p ${environment}
