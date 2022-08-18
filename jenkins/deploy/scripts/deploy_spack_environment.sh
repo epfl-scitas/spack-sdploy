2845f60a8dac83a9bf8d9ac410139349973ea8d7
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-18 17:01:27 +0200

    unrecognized arguments

--- jenkins/deploy/scripts/deploy_spack_environment.sh
+++ jenkins/deploy/scripts/deploy_spack_environment.sh
@@ -4,10 +4,10 @@ echo 'Activating Spack'
 . $JENKINS/activate_spack.sh
 
 echo "Installing stack configuration: ${environment}"
-spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment} -v --log-format=junit --log-file=compilers.${environment}.xml
+spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment}
 
 echo "Installing modules configuration: ${environment}"
-spack --env ${environment} write-modules-yaml -s ${STACK_RELEASE} -p ${environment} -v --log-format=junit --log-file=compilers.${environment}.xml
+spack --env ${environment} write-modules-yaml -s ${STACK_RELEASE} -p ${environment}
 
 echo "Installing packages configuration: ${environment}"
-spack --env ${environment} write-packages-yaml -s ${STACK_RELEASE} -p ${environment} -v --log-format=junit --log-file=compilers.${environment}.xml
+spack --env ${environment} write-packages-yaml -s ${STACK_RELEASE} -p ${environment}
