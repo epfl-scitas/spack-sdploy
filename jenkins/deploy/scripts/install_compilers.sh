a73bafb92963ec1527f7bdeea77093051b906f77
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-18 11:35:12 +0200

    add intel license procedure

--- jenkins/deploy/scripts/install_compilers.sh
+++ jenkins/deploy/scripts/install_compilers.sh
@@ -3,6 +3,14 @@
 echo 'Activating Spack'
 . $JENKINS/activate_spack.sh
 
-echo "Running in environment: ${environment}"
+echo 'Installing License'
+# If diectory already exists, do nothing
+SOURCE_PATH=${SPACK_SDPLOY_INSTALL_PATH}/external/licenses/intel
+LICENSE_PATH=${SPACK_INSTALL_PATH}/etc/spack/licenses/intel
+if [ ! -d ${LICENSE_PATH} ]; then
+    mkdir -p ${LICENSE_PATH}
+    cp ${SOURCE_PATH}/USE_SERVER.lic ${LICENSE_PATH}/license.lic
+fi
 
-spack --env ${environment} install-c -s stacks/${STACK_RELEASE}/${STACK_RELEASE}.yaml
+echo "Installing compilers in environment: ${environment}"
+spack --env ${environment} install-compilers -s stacks/${STACK_RELEASE}/${STACK_RELEASE}.yaml
