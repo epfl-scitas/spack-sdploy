70b9fa17547a0cc4bf59a1bda21d0b5cc0c9722c
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-18 13:23:24 +0200

    fix install-compilers command arguments

--- jenkins/deploy/scripts/install_compilers.sh
+++ jenkins/deploy/scripts/install_compilers.sh
@@ -4,13 +4,17 @@ echo 'Activating Spack'
 . $JENKINS/activate_spack.sh
 
 echo 'Installing License'
-# If diectory already exists, do nothing
+# If directory already exists, remove it
 SOURCE_PATH=${SPACK_SDPLOY_INSTALL_PATH}/external/licenses/intel
 LICENSE_PATH=${SPACK_INSTALL_PATH}/etc/spack/licenses/intel
 if [ ! -d ${LICENSE_PATH} ]; then
     mkdir -p ${LICENSE_PATH}
     cp ${SOURCE_PATH}/USE_SERVER.lic ${LICENSE_PATH}/license.lic
+else
+    rm -r ${LICENSE_PATH}
+    mkdir -p ${LICENSE_PATH}
+    cp ${SOURCE_PATH}/USE_SERVER.lic ${LICENSE_PATH}/license.lic
 fi
 
 echo "Installing compilers in environment: ${environment}"
-spack --env ${environment} install-compilers -s stacks/${STACK_RELEASE}/${STACK_RELEASE}.yaml
+spack --env ${environment} install-compilers -s ${STACK_RELEASE} -p ${environment}
