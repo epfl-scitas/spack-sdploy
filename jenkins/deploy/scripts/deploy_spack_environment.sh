dba3250de7513f84b88d66ac8cbfc4bf35825d73
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-19 11:15:43 +0200

    add debug info

--- jenkins/deploy/scripts/deploy_spack_environment.sh
+++ jenkins/deploy/scripts/deploy_spack_environment.sh
@@ -4,10 +4,10 @@ echo 'Activating Spack'
 . $JENKINS/activate_spack.sh
 
 echo "Installing stack configuration: ${environment}"
-spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment}
+spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment} -d
 
 echo "Installing packages configuration: ${environment}"
-spack write-packages-yaml -s ${STACK_RELEASE} -p ${environment}
+spack write-packages-yaml -s ${STACK_RELEASE} -p ${environment} -d
 
 echo "Installing modules configuration: ${environment}"
-spack write-modules-yaml -s ${STACK_RELEASE} -p ${environment}
+spack write-modules-yaml -s ${STACK_RELEASE} -p ${environment} -d
