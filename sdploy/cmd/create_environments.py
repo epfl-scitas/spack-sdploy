d94a4a1eceb6281e061e73782a48057fba4bfdd1
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-19 13:22:31 +0200

    add config class to create-environments command

--- sdploy/cmd/create_environments.py
+++ sdploy/cmd/create_environments.py
@@ -29,14 +29,47 @@ level = "short"
 
 from ..util import *
 from ..config import *
+from ..config_manager import Config
+
+def setup_parser(subparser):
+    # spack-sdploy will look for a stack after the name given in the parameter
+    # stack under the stacks directory. If it doesn't find, it will assume that
+    # the parameter stack is a fully qualified file name to a stack.yaml file.
+    subparser.add_argument(
+        '-s', '--stack',
+        help='path to the stack file'
+    )
+    subparser.add_argument(
+        '-p', '--platform',
+        help='path to the platform file.'
+    )
+    subparser.add_argument(
+        '--prefix', type=str,
+        help='path to the stacks directory.'
+    )
+    subparser.add_argument(
+        '-d', '--debug', action='store_true', default=False,
+        help='print debug information.'
+    )
 
 def create_environments(parser, args):
     """Create environments"""
 
+    # spack-sdploy setup
+    config = Config(args)
+    if config.debug:
+        config.info()
+
     envs_list = ReadYaml()
-    envs_list.read(fs.join_path(get_prefix(), 'stacks/common.yaml'))
+    envs_list.read(fs.join_path(config.commons_yaml))
+
+    tty.info(f'The following environments were found in commons.yaml:')
+    for e in envs_list.data['environments']:
+        print(f'- {e}')
 
     for e in envs_list.data['environments']:
         if e not in ev.all_environment_names():
-            tty.info(f'Environment {e} not found')
+            tty.info(f'Environment {e} not found in spack.')
             _env_create(e)
+        else:
+            tty.info(f'Environment {e} already exists in spack, passing.')
