291ef1574df98b0217ce9b5c364411b062044920
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-21 10:30:54 +0200

    Remove debug info

--- jenkins/deploy/scripts/deploy_spack_environment.sh
+++ jenkins/deploy/scripts/deploy_spack_environment.sh
@@ -4,13 +4,13 @@ echo 'Activating Spack'
 . $JENKINS/activate_spack.sh
 
 echo "Installing stack configuration: ${environment}"
-spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment} -d
+spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment} 
 
 echo "Installing packages configuration: ${environment}"
-spack write-packages-yaml -s ${STACK_RELEASE} -p ${environment} -d
+spack write-packages-yaml -s ${STACK_RELEASE} -p ${environment}
 
 echo "Installing modules configuration: ${environment}"
 spack write-modules-yaml -s ${STACK_RELEASE} -p ${environment} -d
 
 echo "Installing modules configuration: ${environment}"
-spack write-repos-yaml -s ${STACK_RELEASE} -p ${environment} -d
+spack write-repos-yaml -s ${STACK_RELEASE} -p ${environment}
