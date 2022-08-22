6fa6d3a4104097ace2ec154391a9316c479e4249
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-22 17:35:21 +0200

    Read compilers from stack file

--- sdploy/cmd/s_location.py
+++ sdploy/cmd/s_location.py
@@ -12,9 +12,13 @@ import llnl.util.tty as tty
 import spack.cmd
 import spack.cmd.common.arguments as arguments
 import spack.environment as ev
+
 # import spack.paths
-#import spack.repo
-#import spack.stage
+# import spack.repo
+# import spack.stage
+
+from ..config_manager import Config
+from ..read_leafs import ReadLeaf
 
 description = "print out the location of a single spec"
 section = "Sdploy"
@@ -23,30 +27,58 @@ level = "short"
 from pdb import set_trace as st
 
 def setup_parser(subparser):
-    global directories
-    directories = subparser.add_mutually_exclusive_group()
 
-    directories.add_argument(
-        '-i', '--install-dir', action='store_true',
-        help="install prefix for spec (spec need not be installed)")
-
-    arguments.add_common_arguments(subparser, ['spec'])
+    subparser.add_argument(
+        '-s', '--stack',
+        help='path to the stack file'
+    )
+    subparser.add_argument(
+        '-p', '--platform',
+        help='path to the platform file.'
+    )
+    subparser.add_argument(
+        '--prefix', type=str,
+        help='path to the stacks directory.'
+    )
+    subparser.add_argument(
+        '-d', '--debug', action='store_true', default=False,
+        help='print debug information.'
+   )
+   
+#    global directories
+#    directories = subparser.add_mutually_exclusive_group()
+#
+#    directories.add_argument(
+#        '-i', '--install-dir', action='store_true',
+#        help="install prefix for spec (spec need not be installed)")
+#
+#    arguments.add_common_arguments(subparser, ['spec'])
 
 
 def s_location(parser, args):
 
-    specs = spack.cmd.parse_specs(args.spec)
+    config = Config(args)
+    if config.debug:
+        config.info()
 
-    if not specs:
-        tty.die("You must supply a spec.")
+    # Instantiate class
+    compiler_specs = ReadLeaf(config.platform_yaml, config.stack_yaml, config.debug)
 
-    if len(specs) != 1:
-        tty.die("Too many specs.  Supply only one.")
+    # Gather leafs from tree
+    compiler_specs.read_key('compiler')
 
-    # install_dir command matches against installed specs.
-    if args.install_dir:
+    for compiler in compiler_specs.leafs:
+
+        args.spec = compiler
+        
+        # END OF SPACK-SDPLOY CODE
+        # -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+        specs = spack.cmd.parse_specs(args.spec)
+        
+        # install_dir command matches against installed specs.
         env = ev.active_environment()
         spec = spack.cmd.disambiguate_spec(specs[0], env) # Returs the concrete spec
         print(spec.prefix)
-        return
+    
 
