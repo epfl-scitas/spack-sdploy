250fe6716e3500a36ce9a8332f2245b0a3576ec9
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-23 10:39:34 +0200

    Remove duplicated write-repos-yaml command

--- jenkins/deploy/scripts/deploy_spack_environment.sh
+++ jenkins/deploy/scripts/deploy_spack_environment.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -l
 set -euo pipefail
 
-echo 'Activating Spack'
+# Activating Spack
 . $JENKINS/activate_spack.sh
 
 echo "Installing stack configuration: ${environment}"
-spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment} 
+spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment}
 
 echo "Installing packages configuration: ${environment}"
 spack write-packages-yaml -s ${STACK_RELEASE} -p ${environment}
 
 echo "Installing modules configuration: ${environment}"
-spack write-modules-yaml -s ${STACK_RELEASE} -p ${environment} -d
+spack write-modules-yaml -s ${STACK_RELEASE} -p ${environment}
 
-echo "Installing modules configuration: ${environment}"
-spack write-repos-yaml -s ${STACK_RELEASE} -p ${environment}
+#echo "Installing repos configuration: ${environment}"
+#spack write-repos-yaml -s ${STACK_RELEASE} -p ${environment}
