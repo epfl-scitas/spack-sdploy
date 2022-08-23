11c661cd4d4e1dbd4bdad4a59cbec6ed6f930c79
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-23 19:30:57 +0200

    exclude spec from mirror

--- jenkins/deploy/scripts/add_mirror.sh
+++ jenkins/deploy/scripts/add_mirror.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 . $JENKINS/activate_spack.sh
 
 echo "Creating mirror for environment ${environment}"
-spack --env ${environment} add-mirror create -D -a -s ${STACK_RELEASE} -p ${environment}
+spack --env ${environment} add-mirror create -D -a -s ${STACK_RELEASE} -p ${environment} --exclude-specs openjdk@1.8.0.342.b07-2
