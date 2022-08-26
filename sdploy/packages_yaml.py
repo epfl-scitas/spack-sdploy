6d8cb4ee9034f8de4ef924657e0e876260518cd9
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-26 11:19:15 +0200

    Add debug info

--- sdploy/packages_yaml.py
+++ sdploy/packages_yaml.py
@@ -91,13 +91,15 @@ class PackagesYaml(StackFile):
     def _packages_yaml_packages_version(self, version_attributes):
         """Adds version to dictionary"""
 
+        tty.debug(f'Entering function: {inspect.stack()[0][3]}')
         version = self._handle_filter(version_attributes)
-
         return version
 
 
     def _packages_yaml_packages_variants(self, variants_attributes):
         """Adds variants to dictionary"""
+
+        tty.debug(f'Entering function: {inspect.stack()[0][3]}')
 
         variants = []
         if 'common' in variants_attributes:
@@ -109,6 +111,7 @@ class PackagesYaml(StackFile):
     def _packages_yaml_packages_buildable(self, buildable):
         """Adds version to dictionary"""
 
+        tty.debug(f'Entering function: {inspect.stack()[0][3]}')
         return buildable
 
 
