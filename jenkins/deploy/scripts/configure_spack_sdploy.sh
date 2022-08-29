d631436d868814fd23e72671f65753ed6127f01c
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-29 10:06:40 +0200

    replace spack_user_config_path with system

--- jenkins/deploy/scripts/configure_spack_sdploy.sh
+++ jenkins/deploy/scripts/configure_spack_sdploy.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 echo 'Activating Python virtual environment'
 . ${PYTHON_VIRTUALENV_PATH}/bin/activate
 
-if [ -e ${SPACK_USER_CONFIG_PATH} ]; then
+if [ -e ${SPACK_SYSTEM_CONFIG_PATH} ]; then
     echo 'Previous configuration directory detected, removing...'
-    rm -rf ${SPACK_USER_CONFIG_PATH}
+    rm -rf ${SPACK_SYSTEM_CONFIG_PATH}
 fi
 
-mkdir ${SPACK_USER_CONFIG_PATH}
-cat > ${SPACK_USER_CONFIG_PATH}/config.yaml << EOF
+mkdir ${SPACK_SYSTEM_CONFIG_PATH}
+cat > ${SPACK_SYSTEM_CONFIG_PATH}/config.yaml << EOF
 config:
   extensions:
   - ${SPACK_SDPLOY_INSTALL_PATH}
