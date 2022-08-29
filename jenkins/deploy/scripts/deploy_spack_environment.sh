f3562547cd868ea223de91861854b429ec2ab21d
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-29 10:14:10 +0200

    write spack.yaml and the remaining configuration in two scripts

--- jenkins/deploy/scripts/deploy_spack_environment.sh
+++ jenkins/deploy/scripts/deploy_spack_environment.sh
@@ -9,11 +9,11 @@ environment=$(echo $NODE_LABELS | cut -d '-' -f 1)
 echo "Installing stack configuration: ${environment}"
 spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment}
 
-echo "Installing packages configuration: ${environment}"
-spack -v write-packages-yaml -s ${STACK_RELEASE} -p ${environment} -d
-
-echo "Installing modules configuration: ${environment}"
-spack -v write-modules-yaml -s ${STACK_RELEASE} -p ${environment} -d
-
-echo "Installing mirrors configuration: ${environment}"
-spack -v write-mirrors-yaml -s ${STACK_RELEASE} -p ${environment} -d
+# echo "Installing packages configuration: ${environment}"
+# spack -v write-packages-yaml -s ${STACK_RELEASE} -p ${environment} -d
+#
+# echo "Installing modules configuration: ${environment}"
+# spack -v write-modules-yaml -s ${STACK_RELEASE} -p ${environment} -d
+#
+# echo "Installing mirrors configuration: ${environment}"
+# spack -v write-mirrors-yaml -s ${STACK_RELEASE} -p ${environment} -d
