fbfafff580e20beef9672654379caf357440cbf2
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-30 14:30:43 +0200

    Refer SPACK_SYSTEM_CONFIG_PATH to environment directory

--- jenkins/deploy/scripts/deploy_spack_environment.sh
+++ jenkins/deploy/scripts/deploy_spack_environment.sh
@@ -8,17 +8,19 @@ echo "ENVIRONMENT: $environment"
 # Activating Spack
 . $JENKINS/activate_spack.sh
 
+SPACK_SYSTEM_CONFIG_PATH=$(spack location -e $environment)
+
 echo "Installing stack configuration: ${environment}"
 spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment}
 
 echo "Installing packages configuration: ${environment}"
-spack -v --env ${environment} write-packages-yaml -s ${STACK_RELEASE} -p ${environment} -d
+spack --env ${environment} write-packages-yaml -s ${STACK_RELEASE} -p ${environment} -d
 
 echo "Installing modules configuration: ${environment}"
-spack -v --env ${environment} write-modules-yaml -s ${STACK_RELEASE} -p ${environment} -d
+spack --env ${environment} write-modules-yaml -s ${STACK_RELEASE} -p ${environment} -d
 
 echo "Installing mirrors configuration: ${environment}"
-spack -v --env ${environment} write-mirrors-yaml -s ${STACK_RELEASE} -p ${environment} -d
+spack --env ${environment} write-mirrors-yaml -s ${STACK_RELEASE} -p ${environment} -d
 
 echo "Installing config.yaml configuration: ${environment}"
-spack -v --env ${environment} write-config-yaml -s ${STACK_RELEASE} -p ${environment} -d
+spack --env ${environment} write-config-yaml -s ${STACK_RELEASE} -p ${environment} -d
