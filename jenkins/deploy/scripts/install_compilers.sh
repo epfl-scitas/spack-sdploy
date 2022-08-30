ea68a9500e3bac7d81f726b1c2f13c5e94321009
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-08-30 13:44:19 +0200

    Add debug info

--- jenkins/deploy/scripts/install_compilers.sh
+++ jenkins/deploy/scripts/install_compilers.sh
@@ -50,13 +50,13 @@ do
     spack compiler find --scope system ${spec_path}
 done <<< $(cat compilers-perline.${environment})
 
+echo "spack  blame compilers before sed:"
 spack config blame compilers
 
 sed -i 's/intel@19.1.3.304/intel@20.0.4/' ${SPACK_SYSTEM_CONFIG_PATH}/compilers.yaml
 
+echo "spack  blame compilers after sed:"
 spack config blame compilers
 
 echo "Adding system compiler I"
 spack compiler find --scope system
-echo "Adding system compiler II"
-spack compiler find --scope system /usr/bin/gcc
