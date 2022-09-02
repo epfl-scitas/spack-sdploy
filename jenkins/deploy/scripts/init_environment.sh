c388dfb8483763ff02645e5a80b1d7a5e80f59ad
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-09-02 21:03:54 +0200

    copy spack-packagelist

--- jenkins/deploy/scripts/init_environment.sh
+++ jenkins/deploy/scripts/init_environment.sh
@@ -12,7 +12,7 @@ echo "Who is Spack:"
 spack location -r
 
 echo "Processing environment: ${environment}"
-if [[ -z $(spack env list | grep $environment) && $? -eq 1 ]] ; then
+if [ ! -e ${SPACK_INSTALL_PATH}/var/spack/environments/${environment}/spack.yaml ]; then
     echo "$environment: creating..."
     spack env create ${environment}
 else
@@ -21,3 +21,11 @@ fi
 
 echo "List spack environments:"
 spack env list
+
+# echo "Processing environment: ${environment}"
+# if [[ -z $(spack env list | grep $environment) && $? -eq 1 ]] ; then
+#     echo "$environment: creating..."
+#     spack env create ${environment}
+# else
+#     echo "$environment: found"
+# fi
