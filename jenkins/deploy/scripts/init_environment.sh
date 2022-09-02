6378bf5fb05fd326143eff5158fb5c29013ddace
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-09-02 19:07:05 +0200

    Simplify enviroment existence logic

--- jenkins/deploy/scripts/init_environment.sh
+++ jenkins/deploy/scripts/init_environment.sh
@@ -7,10 +7,18 @@ echo "ENVIRONMENT ${environment}"
 echo 'Activating Spack'
 . $JENKINS/activate_spack.sh
 
-echo "Initialize environment: ${environment}"
-if [[ -z $(spack env list | grep $environment) ]] && $?; then
-    echo "Creating environment $environment"
-    spack env create ${environment}
+if [ -d $(spack location -e ${environment}) ]; then
+    echo "Environment ${environment} already exists"
 else
-    echo "Found environment $environment"
+    echo "Creating environment ${environment}"
+    spack env create ${environment}
 fi
+
+# echo "Initialize environment: ${environment}"
+# if [[ -z $(spack env list | grep $environment) ]] && $?; then
+#     echo "Creating environment $environment"
+#     spack env create ${environment}
+# else
+#     echo "Found environment $environment"
+# fi
+
