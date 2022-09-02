b6b81d6e0d475a7b86dd142fb187f431ca5637eb
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-09-02 23:06:11 +0200

    Correct intel version

--- jenkins/deploy/scripts/deploy_common_resources.sh
+++ jenkins/deploy/scripts/deploy_common_resources.sh
@@ -16,4 +16,16 @@ echo "Installing mirrors configuration"
 spack write-mirrors-yaml -s ${STACK_RELEASE} -p ph02_avx
 spack config blame mirrors
 
-cp ${SPACK_SYSTEM_CONFIG_PATH}/compilers.yaml ${SPACK_INSTALL_PATH}/etc/spack
+echo "Installing compilers.yaml"
+SOURCE=${SPACK_SYSTEM_CONFIG_PATH}/compilers.yaml
+DEST=${SPACK_INSTALL_PATH}/etc/spack
+cp ${SOURCE} ${DEST}
+
+echo "Fixing intel compiler version"
+spack_bin=$(spack location -r)/bin/spack
+eval `$spack_bin load --sh intel@20.0.4`
+bad_ver=$(icc --version |grep icc |cut -d ' ' -f 3)
+
+sed -i "s/intel@${bad_ver}/intel@20.0.4/" ${DEST}/compilers.yaml
+
+cat ${DEST}/compilers.yaml
