4fad68ed99cde1dfb57044da270745fc7e15d6e2
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-09-03 00:19:24 +0200

    Add install stack stage

--- jenkins/deploy/scripts/install_stack.sh
+++ jenkins/deploy/scripts/install_stack.sh
@@ -2,12 +2,14 @@
 set -euo pipefail
 
 environment=$(echo $NODE_LABELS | cut -d '-' -f 1)
+echo "NODE_LABEL: $NODE_LABELS"
+echo "ENVIRONMENT: $environment"
 
 # Activating Spack
 . $JENKINS/activate_spack.sh
 
 echo "Installing packages for environment ${environment}"
-spack --env ${environment} install --fail-fast
+spack --env ${environment} install
 
 echo "Creating modules for environment ${environment}"
 spack --env ${environment} module lmod refresh -y
