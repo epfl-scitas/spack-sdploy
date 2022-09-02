c9117109c9b7c1a59a73a0387c5c1e332557661a
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-09-02 15:26:56 +0200

    Add install compilers stage

--- jenkins/deploy/scripts/install_compilers.sh
+++ jenkins/deploy/scripts/install_compilers.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 environment=$(echo $NODE_LABELS | cut -d '-' -f 1)
 echo "ENVIRONMENT ${environment}"
 
-echo 'Activating Spack'
+# Activate spack
 . $JENKINS/activate_spack.sh
 
 echo 'Installing License'
@@ -30,53 +30,42 @@ echo "Contents of compilers variable:"
 compilers=$(cat compilers-inline.${environment})
 echo $compilers
 
-echo "Reporting BEFORE installing compilers:"
-cat $(spack location -e $environment)/spack.yaml
+# echo "Reporting BEFORE installing compilers:"
+# cat $(spack location -e $environment)/spack.yaml
 
 echo "Installing compilers"
 spack -v --env ${environment} install ${compilers}
 
-echo "Reporting BEFORE modules generation:"
-cat $(spack location -e $environment)/spack.yaml
+# echo "Reporting BEFORE modules generation:"
+# cat $(spack location -e $environment)/spack.yaml
 
-echo "Add seen in spack-packagelist"
-spack -v --env ${environment} module lmod refresh -y ${compilers}
+# echo "Add seen in spack-packagelist"
+# spack -v --env ${environment} module lmod refresh -y ${compilers}
 
-echo "Reporting BEFORE while loop:"
-cat $(spack location -e $environment)/spack.yaml
+# echo "Reporting BEFORE while loop:"
+# cat $(spack location -e $environment)/spack.yaml
 
 echo "Adding stack compilers"
+
 while read -r line
 do
     spec_path=$(spack location -i ${line})
     spack compiler find --scope system ${spec_path}
 done <<< $(cat compilers-perline.${environment})
 
-echo "Reporting AFTER while loop:"
-cat $(spack location -e $environment)/spack.yaml
 
-echo "spack  blame compilers before sed:"
-spack config blame compilers
-
-sed -i 's/intel@19.1.3.304/intel@20.0.4/' ${SPACK_SYSTEM_CONFIG_PATH}/compilers.yaml
-
-echo "spack  blame compilers after sed:"
-spack config blame compilers
-
-echo "Reporting BEFORE system compiler I:"
-# already has the extra compiler lines in spack.yaml
-cat $(spack location -e $environment)/spack.yaml
-
-echo "Adding system compiler I"
-spack compiler find --scope system
-cat $(spack location -e ${environment})/spack.yaml
-
-echo "Reporting BEFORE system compiler II:"
-cat $(spack location -e $environment)/spack.yaml
-
-#echo "Adding system compiler II"
-#spack compiler find --env
-#cat $(spack location -e ${environment})/spack.yaml
-
-echo "Reporting AT THE END:"
-cat $(spack location -e $environment)/spack.yaml
+# echo "Reporting AFTER while loop:"
+# cat $(spack location -e $environment)/spack.yaml
+#
+# echo "spack  blame compilers before sed:"
+# spack config blame compilers
+#
+# sed -i 's/intel@19.1.3.304/intel@20.0.4/' ${SPACK_SYSTEM_CONFIG_PATH}/compilers.yaml
+#
+# echo "spack  blame compilers after sed:"
+# spack config blame compilers
+#
+# echo "Reporting BEFORE system compiler I:"
+# # already has the extra compiler lines in spack.yaml
+# cat $(spack location -e $environment)/spack.yaml
+#
