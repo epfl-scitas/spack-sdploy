7c80c97489b263437040b5612c4362755263af4a
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-09-02 14:01:59 +0200

    command type

--- jenkins/deploy/scripts/deploy_spack_environment.sh
+++ jenkins/deploy/scripts/deploy_spack_environment.sh
@@ -11,7 +11,7 @@ echo "ENVIRONMENT: $environment"
 export SPACK_SYSTEM_CONFIG_PATH=${SYSTEM_CONFIG_PREFIX}/${environment}
 
 echo "Creating spack environemnt: ${environment}"
-spack env create env ${environment}
+spack env create ${environment}
 
 echo "Deploying manifest"
 spack --env ${environment} write-spack-yaml -s ${STACK_RELEASE} -p ${environment}
@@ -27,8 +27,5 @@ spack --env ${environment} config blame modules
 echo "Installing config.yaml configuration: ${environment}"
 spack --env ${environment} write-config-yaml -s ${STACK_RELEASE} -p ${environment} -d
 spack --env ${environment} config blame config
-
-#echo "Installing mirrors configuration: ${environment}"
-#spack --env ${environment} write-mirrors-yaml -s ${STACK_RELEASE} -p ${environment} -d
 
 
