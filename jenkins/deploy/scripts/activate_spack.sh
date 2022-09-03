9bef718e2e005f94a371861217576756480b683e
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-09-03 08:38:56 +0200

    Update echos

--- jenkins/deploy/scripts/activate_spack.sh
+++ jenkins/deploy/scripts/activate_spack.sh
@@ -4,8 +4,7 @@ set -euo pipefail
 echo 'Activating Python virtual environment'
 . ${PYTHON_VIRTUALENV_PATH}/bin/activate
 
-echo 'Source Spack'
+echo 'Active Spack'
 . $SPACK_INSTALL_PATH/share/spack/setup-env.sh
 
-echo "Active spack:"
 spack --version
