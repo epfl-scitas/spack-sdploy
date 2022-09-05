78176d6afc7ab3a1ef96ab9dc8a0ac13c0241a54
Nicolas Richart <nicolas.richart@epfl.ch>
2022-09-05 15:56:02 +0200

    simplifying

--- jenkins/deploy/scripts/init_environment.sh
+++ jenkins/deploy/scripts/init_environment.sh
@@ -19,26 +19,26 @@ echo "List spack environments:"
 spack env list
 
 echo "Deploying manifest"
-spack --env ${environment} -d write-spack-yaml -s ${STACK_RELEASE} -p ${environment}
+spack --env ${environment} -d write-spack-yaml -s ${STACK_RELEASE}
 
 echo "Installing packages configuration"
-spack --env ${environment} -d write-packages-yaml -s ${STACK_RELEASE} -p ${environment}
+spack --env ${environment} -d write-packages-yaml -s ${STACK_RELEASE}
 spack --env ${environment} config blame packages
 
 echo "Installing modules configuration"
-spack --env ${environment} -d write-modules-yaml -s ${STACK_RELEASE} -p ${environment}
+spack --env ${environment} -d write-modules-yaml -s ${STACK_RELEASE}
 spack --env ${environment} config blame modules
 
 echo "Installing config.yaml configuration: ${environment}"
-spack --env ${environment} -d write-config-yaml -s ${STACK_RELEASE} -p ${environment}
+spack --env ${environment} -d write-config-yaml -s ${STACK_RELEASE}
 spack --env ${environment} config blame config
 
 echo "Installing external repos for:"
-spack --env ${environment} write-repos-yaml -s ${STACK_RELEASE} -p ${environment}
+spack --env ${environment} write-repos-yaml -s ${STACK_RELEASE}
 spack --env ${environment} config blame repos
 
 echo "Installing mirrors configuration"
-spack --env ${environment} write-mirrors-yaml -s ${STACK_RELEASE} -p ${environment}
+spack --env ${environment} write-mirrors-yaml -s ${STACK_RELEASE}
 spack --env ${environment} config blame mirrors
 
 echo "List environment directory contents"
