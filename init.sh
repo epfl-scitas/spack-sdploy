7b9e3c51840e14224f3e5d3e3d6b8b01f0ba0574
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-11-14 21:04:24 +0100

    Add logs to each step

--- init.sh
+++ init.sh
@@ -67,29 +67,108 @@ echo "STACK: ${STACK}"
 echo "ENVIRONMENT: ${ENVIRONMENT}"
 echo "NODE_LABELS: ${NODE_LABELS}"
 
-# STEP 1
-${_PREFIX}/update_production_configuration.sh
+LOGS=${WORK_DIR}/logs
+mkdir -p ${LOGS}
 
-# STEP 2
-${_PREFIX}/install_spack.sh
+execution_timestamp=`date +%y%m%d.%k%m%M`
+echo "Execution timestamp: ${execution_timestamp}"
+echo
+echo '  _________________________________________   ____ '
+echo ' /   _____/\__    ___/\_   _____/\______   \ /_   |'
+echo ' \_____  \   |    |    |    __)_  |     ___/  |   |'
+echo ' /        \  |    |    |        \ |    |      |   |'
+echo '/_______  /  |____|   /_______  / |____|      |___|'
+echo '        \/                    \/                   '
+echo
+echo > update_production_configuration
+echo
+${_PREFIX}/update_production_configuration.sh | tee ${LOGS}/01_update_production_configuration.${execution_timestamp}.log
 
-# STEP 3
-${_PREFIX}/install_spack_sdploy.sh
+echo '  _________________________________________  ________  '
+echo ' /   _____/\__    ___/\_   _____/\______   \ \_____  \ '
+echo ' \_____  \   |    |    |    __)_  |     ___/  /  ____/ '
+echo ' /        \  |    |    |        \ |    |     /       \ '
+echo '/_______  /  |____|   /_______  / |____|     \_______ \'
+echo '        \/                    \/                     \/'
+echo
+echo '> install_spack.sh'
+echo
+${_PREFIX}/install_spack.sh | tee ${LOGS}/02_install_spack.${execution_timestamp}.log
 
-# STEP 4
-${_PREFIX}/clone_external_repos.sh
+echo '  _________________________________________  ________  '
+echo ' /   _____/\__    ___/\_   _____/\______   \ \_____  \ '
+echo ' \_____  \   |    |    |    __)_  |     ___/   _(__  < '
+echo ' /        \  |    |    |        \ |    |      /       \'
+echo '/_______  /  |____|   /_______  / |____|     /______  /'
+echo '        \/                    \/                    \/ '
+echo
+echo '> install_spack_sdploy.sh'
+echo
+${_PREFIX}/install_spack_sdploy.sh | tee ${LOGS}/03_install_spack_sdploy.${execution_timestamp}.log
 
-# STEP 5
-${_PREFIX}/init_environment.sh
 
-# STEP 6
-${_PREFIX}/install_compilers_parallel.sh
+echo '  _________________________________________     _____  '
+echo ' /   _____/\__    ___/\_   _____/\______   \   /  |  | '
+echo ' \_____  \   |    |    |    __)_  |     ___/  /   |  |_'
+echo ' /        \  |    |    |        \ |    |     /    ^   /'
+echo '/_______  /  |____|   /_______  / |____|     \____   | '
+echo '        \/                    \/                  |__| '
+echo
+echo > clone_external_repos.sh
+echo
+${_PREFIX}/clone_external_repos.sh | tee ${LOGS}/04_clone_external_repos.${execution_timestamp}.log
 
-# STEP 7
-${_PREFIX}/concretize.sh
+echo '  _________________________________________   .________'
+echo ' /   _____/\__    ___/\_   _____/\______   \  |   ____/'
+echo ' \_____  \   |    |    |    __)_  |     ___/  |____  \ '
+echo ' /        \  |    |    |        \ |    |      /       \'
+echo '/_______  /  |____|   /_______  / |____|     /______  /'
+echo '        \/                    \/                    \/ '
+echo
+echo '> init_environment.sh'
+echo
+${_PREFIX}/init_environment.sh | tee ${LOGS}/05_init_environment.${execution_timestamp}.log
 
-# STEP 8
-${_PREFIX}/add_mirror.sh
+echo '  _________________________________________    ________'
+echo ' /   _____/\__    ___/\_   _____/\______   \  /  _____/'
+echo ' \_____  \   |    |    |    __)_  |     ___/ /   __  \ '
+echo ' /        \  |    |    |        \ |    |     \  |__\  \'
+echo '/_______  /  |____|   /_______  / |____|      \_____  /'
+echo '        \/                    \/                    \/ '
+echo
+echo '> install_compilers_parallel.sh'
+echo
+${_PREFIX}/install_compilers_parallel.sh | tee ${LOGS}/06_install_compilers_parallel.${execution_timestamp}.log
 
-# STEP 9
-${_PREFIX}/install_stack.sh
+echo '  _________________________________________  _________ '
+echo ' /   _____/\__    ___/\_   _____/\______   \ \______  \'
+echo ' \_____  \   |    |    |    __)_  |     ___/     /    /'
+echo ' /        \  |    |    |        \ |    |        /    / '
+echo '/_______  /  |____|   /_______  / |____|       /____/  '
+echo '        \/                    \/                       '
+echo
+echo '> concretize.sh'
+echo
+${_PREFIX}/concretize.sh | tee ${LOGS}/07_concretize.${execution_timestamp}.log
+
+echo '  _________________________________________    ______  '
+echo ' /   _____/\__    ___/\_   _____/\______   \  /  __  \ '
+echo ' \_____  \   |    |    |    __)_  |     ___/  >      < '
+echo ' /        \  |    |    |        \ |    |     /   --   \'
+echo '/_______  /  |____|   /_______  / |____|     \______  /'
+echo '        \/                    \/                    \/ '
+echo
+echo '> add_mirror.sh'
+echo
+${_PREFIX}/add_mirror.sh | tee ${LOGS}/08_add_mirror.${execution_timestamp}.log
+
+echo '  _________________________________________   ________ '
+echo ' /   _____/\__    ___/\_   _____/\______   \ /   __   \'
+echo ' \_____  \   |    |    |    __)_  |     ___/ \____    /'
+echo ' /        \  |    |    |        \ |    |        /    / '
+echo '/_______  /  |____|   /_______  / |____|       /____/  '
+echo '        \/                    \/                       '
+echo
+echo '> install_stack.sh'
+echo
+${_PREFIX}/install_stack.sh | tee ${LOGS}/09_install_stack.${execution_timestamp}.log
