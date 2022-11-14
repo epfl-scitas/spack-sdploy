b0d1007b0008617162241b488ad84106d72af1a6
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-11-14 15:26:29 +0100

    Export concretize output to log file

--- jenkins/deploy/scripts/concretize.sh
+++ jenkins/deploy/scripts/concretize.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 # Activating Spack
 . $JENKINS/activate_spack.sh
 
+timestamp=`date +%y%m%d.%k%m%M`
+
 echo "Concretizing environment ${environment}"
-spack --env ${environment} concretize --force
+spack --env ${environment} concretize --force > concretize.${timestamp}.log
