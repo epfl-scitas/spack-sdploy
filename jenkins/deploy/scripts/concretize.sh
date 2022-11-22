7b0b5193608623d7e8e3e89c86538ceb47b338e9
Eduardo Rothe <eduardo.rothe@epfl.ch>
2022-11-22 16:29:12 +0100

    Remove build_jobs config

--- jenkins/deploy/scripts/concretize.sh
+++ jenkins/deploy/scripts/concretize.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 . $JENKINS/activate_spack.sh
 
 echo "Concretizing environment ${environment}"
-spack -c config:build_jobs:32 --env ${environment} concretize --force
+spack --env ${environment} concretize --force
