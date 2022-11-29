18dc8bf96941d57fa61f4a0540b67ef3b60b5ed3
Nicolas Richart <nicolas.richart@epfl.ch>
2022-11-29 11:33:36 +0100

    More info in case of connexion failure

--- jenkins/deploy/scripts/test_connectivity.sh
+++ jenkins/deploy/scripts/test_connectivity.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 wget -v www.google.ch -O /tmp/index.html
 
 if [ -e /tmp/index.html ]; then
     rm /tmp/index.html
+    exit 0
+else
+    env | grep -i proxy
+    exit -1
 fi
