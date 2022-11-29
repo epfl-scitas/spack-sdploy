72a3416be43e32d4d0f99f7f9a9d9744d033d7f3
Nicolas Richart <nicolas.richart@epfl.ch>
2022-11-29 14:10:04 +0100

    activate envs conditionnaly

--- jenkins/deploy/scripts/activate_spack.sh
+++ jenkins/deploy/scripts/activate_spack.sh
@@ -1,25 +1,28 @@
 #!/bin/bash -l
 set -euo pipefail
 
+source $HOME/.profile
+
 echo 'Activating Python virtual environment'
-. ${PYTHON_VIRTUALENV_PATH}/bin/activate
-
-echo 'Active Spack'
-. $SPACK_INSTALL_PATH/share/spack/setup-env.sh
-
-spack --version
-
-environment=$(echo $NODE_LABELS | cut -d '-' -f 1)
-echo "ENVIRONMENT ${environment}"
-
-_env_path=$(spack location --env ${environment} || true)
-
-export SPACK_USER_CACHE_PATH=$HOME/.spack-${STACK}
-export SPACK_USER_CONFIG_PATH=$HOME/.spack-${STACK}
-
-if [ ! -z ${_env_path} ]; then
-    export SPACK_SYSTEM_CONFIG_PATH=${_env_path}
-    echo "Setting SPACK_SYSTEM_CONFIG_PATH to: ${SPACK_SYSTEM_CONFIG_PATH}"
+if [ -e ${PYTHON_VIRTUALENV_PATH}/bin/activate ]; then
+    . ${PYTHON_VIRTUALENV_PATH}/bin/activate
 fi
 
-source $HOME/.profile
+echo 'Active Spack'
+if [ -e $SPACK_INSTALL_PATH/share/spack/setup-env.sh ]; then
+    . $SPACK_INSTALL_PATH/share/spack/setup-env.sh
+    spack --version
+
+    environment=$(echo $NODE_LABELS | cut -d '-' -f 1)
+    echo "ENVIRONMENT ${environment}"
+
+    _env_path=$(spack location --env ${environment} || true)
+
+    export SPACK_USER_CACHE_PATH=$HOME/.spack-${STACK}
+    export SPACK_USER_CONFIG_PATH=$HOME/.spack-${STACK}
+
+    if [ ! -z ${_env_path} ]; then
+        export SPACK_SYSTEM_CONFIG_PATH=${_env_path}
+        echo "Setting SPACK_SYSTEM_CONFIG_PATH to: ${SPACK_SYSTEM_CONFIG_PATH}"
+    fi
+fi
--- jenkins/deploy/scripts/test_connectivity.sh
+++ jenkins/deploy/scripts/test_connectivity.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+. $JENKINS/activate_spack.sh
+
 wget --timeout=3 -v www.google.ch -O /tmp/index.html
 
 if [ $? -eq 0 ]; then
