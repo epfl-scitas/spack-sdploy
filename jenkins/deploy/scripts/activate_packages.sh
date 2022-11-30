7a83557ec7f948af41b51c4ff09fff5a086499b6
Nicolas Richart <nicolas.richart@epfl.ch>
2022-11-30 15:38:29 +0100

    Activating the python modules

--- jenkins/deploy/scripts/activate_packages.sh
+++ jenkins/deploy/scripts/activate_packages.sh
@@ -10,8 +10,24 @@ spack write-activate-list -s ${STACK_RELEASE} -p ${environment}
 echo "Loop through all packages"
 packages_to_activate=$(cat packages_to_activate)
 
-for package in ${packages_to_activate}
+CACHE_FILE=${HOME}/.${STACK_RELEASE}.${STACK_RELEASE_VER}_activated_cache
+touch ${CACHE_FILE}
+
+set +e
+while read -r spec
 do
-    echo activating package ${package}
-    spack activate ${package}
-done
+    hash=$(spack find -xL "${spec}" | grep -e "[a-z0-9]\{16\}" | awk '{ print $1 }')
+    echo "Activating package ${spec} (${hash})"
+    grep -q ${hash} ${CACHE_FILE}
+    if [ $? -eq 0 ]; then
+        echo "Already activated"
+        continue
+    fi
+
+    spack -e ${environment} activate --force "/${hash}"
+    if [ $? -eq 0 ]; then
+        echo "ACTIVATED ${hash}"
+        echo "$spec ($hash)" >> ${CACHE_FILE}
+    fi
+done <<< $(cat packages_to_activate)
+set -e
