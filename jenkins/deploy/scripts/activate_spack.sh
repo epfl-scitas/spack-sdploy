3f3d67a6eff03a4834ac9aa70df66025e85b706e
Nicolas Richart <nicolas.richart@epfl.ch>
2022-12-01 13:41:41 +0100

    Activate spack must be "silent"

--- jenkins/deploy/scripts/activate_spack.sh
+++ jenkins/deploy/scripts/activate_spack.sh
@@ -5,12 +5,12 @@ set -euo pipefail
 . ${JENKINS}/setenv.sh
 
 
-echo 'Activating Python virtual environment'
+echo 'Activating Python virtual environment' 1>&2
 if [ -e ${PYTHON_VIRTUALENV_PATH}/bin/activate ]; then
     . ${PYTHON_VIRTUALENV_PATH}/bin/activate
 fi
 
-echo 'Active Spack'
+echo 'Active Spack' 1>&2
 if [ -e $SPACK_INSTALL_PATH/share/spack/setup-env.sh ]; then
     . $SPACK_INSTALL_PATH/share/spack/setup-env.sh
     spack --version
@@ -24,17 +24,17 @@ if [ -e $SPACK_INSTALL_PATH/share/spack/setup-env.sh ]; then
     res=$?
     set -e
     if [ res -eq 0 ]; then
-        echo "Running on a login node"
+        echo "Running on a login node" 1>&2
         break
     fi
 
-    echo "ENVIRONMENT ${environment}"
+    echo "ENVIRONMENT ${environment}" 1>&2
 
     _env_path=$(spack location --env ${environment} || true)
 
 
     if [ ! -z ${_env_path} ]; then
         export SPACK_SYSTEM_CONFIG_PATH=${_env_path}
-        echo "Setting SPACK_SYSTEM_CONFIG_PATH to: ${SPACK_SYSTEM_CONFIG_PATH}"
+        echo "Setting SPACK_SYSTEM_CONFIG_PATH to: ${SPACK_SYSTEM_CONFIG_PATH}" 1>&2
     fi
 fi
