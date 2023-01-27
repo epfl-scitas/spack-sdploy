4f7b9487d9437685a4a855cd4b4c074a86c63e27
Eduardo Rothe <eduardo.rothe@epfl.ch>
2023-01-27 15:47:50 +0100

    Change licence path

--- sdploy/config_yaml.py
+++ sdploy/config_yaml.py
@@ -76,8 +76,7 @@ class ConfigYaml(StackFile):
     def _add_license_dir(self):
         self.conf['license_dir'] = os.path.join(self.commons.data['work_directory'],
                                                 self.commons.data['stack_release'],
-                                                self.commons.data['spack_sdploy'],
-                                                'external', 'licenses')       
+                                                self.commons.data['spack_licences'])
 
     def _add_build_stage(self):
         """Add build stages section to the dictionary"""
