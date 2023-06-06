926a92c598719629299d2696e84c85d6129999ac
Nicolas Richart <nicolas.richart@epfl.ch>
2023-06-06 20:22:22 +0200

    install spack does not work on fresh start

--- jenkins/deploy/scripts/install_spack.sh
+++ jenkins/deploy/scripts/install_spack.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 set -euo pipefail
 
-echo 'Activating Python virtual environment'
-. ${JENKINS}/activate_spack.sh
+. ${JENKINS}/setenv.sh
 
 if [ -e ${SPACK_INSTALL_PATH} ]; then
     echo 'Previous installation of Spack detected...'
     cd ${SPACK_INSTALL_PATH}
     git fetch origin
     git checkout $SPACK_RELEASE
+    cd -
 else
     echo 'Spack not detected...'
     git clone -b $SPACK_RELEASE https://github.com/spack/spack ${SPACK_INSTALL_PATH}
@@ -18,9 +18,9 @@ if [ ! -d $HOME/.spack-${STACK} ]; then
     mkdir -p $HOME/.spack-${STACK}
 fi
 
-export SPACK_USER_CACHE_PATH=$HOME/.spack-${STACK}
-export SPACK_USER_CONFIG_PATH=$HOME/.spack-${STACK}
-
+echo 'Activating Python virtual environment'
+pwd
+. ${JENKINS}/activate_spack.sh
 
 echo 'Source Spack and show version'
 . $SPACK_INSTALL_PATH/share/spack/setup-env.sh
