985c6390def06b74e65efcdc6f85ccd6a1c781a2
Nicolas Richart <nicolas.richart@epfl.ch>
2023-06-06 09:17:55 +0200

    Ensure no concretize takes place during the installation

--- jenkins/deploy/scripts/install_stack.sh
+++ jenkins/deploy/scripts/install_stack.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 . $JENKINS/activate_spack.sh
 
 echo "Installing packages for environment ${environment}"
-spack --env ${environment} install --log-format junit --log-file install_stack.xml
+spack --env ${environment} install --log-format junit --log-file install_stack.xml --only-concrete
 
 # Modules are now created in next step 
 #echo "Creating modules for environment ${environment}"
