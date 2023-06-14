b27440320018626563f67400ba125df286bb66a7
Nicolas Richart <nicolas.richart@epfl.ch>
2023-06-14 19:47:27 +0200

    adding platform

--- jenkins/deploy/scripts/get_tokens.sh
+++ jenkins/deploy/scripts/get_tokens.sh
@@ -4,5 +4,10 @@ set -euo pipefail
 # Activating Spack
 . $JENKINS/activate_spack.sh 2> /dev/null
 
-TOKENS=`yq -s '.[0].tokens + .[1].platform.tokens'  stacks/${STACK_RELEASE}/common.yaml stacks/${STACK_RELEASE}/platforms/${environment}.yaml`
+if [ "$#" -eq 2 ]; then
+    TOKENS=`yq -s '.[0].tokens + .[1].platform.tokens' stacks/${STACK_RELEASE}/common.yaml stacks/${STACK_RELEASE}/platforms/$2.yaml`
+else
+    TOKENS=`yq '.tokens' stacks/${STACK_RELEASE}/common.yaml`
+fi
+
 echo $TOKENS | yq -r ".$1"
