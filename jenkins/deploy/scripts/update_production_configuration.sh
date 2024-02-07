d8f41c02016504f22ad312b4ebdd387098c6031e
Nicolas Richart <nicolas.richart@epfl.ch>
2024-02-07 18:16:14 +0100

    Correcting missing jq

--- jenkins/deploy/scripts/update_production_configuration.sh
+++ jenkins/deploy/scripts/update_production_configuration.sh
@@ -16,5 +16,8 @@ pip install -U pip
 echo 'Installing Jinja2'
 pip install jinja2 jq yq
 
+curl -L --output ${PYTHON_VIRTUALENV_PATH}/bin/jq https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64 
+chmod +x ${PYTHON_VIRTUALENV_PATH}/bin/jq
+
 #echo 'Installing yareed'
 #pip install yareed/
